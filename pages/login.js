47cc02683909d32c9e633ca3c4c877685fd7d411
= <patrickjosephfleming@gmail.com>
2020-12-18 14:16:43 +0000

    login page created

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+// pages/index.js
+
+const Login = () => (
+  <div>
+    <h1>Login page</h1>
+  </div>
+);
+export default Login;
+
+{
+  /* <input type='text'>Email</input>
+    <input type='text'>Password</input>
+    <button>Register</button> */
+}
