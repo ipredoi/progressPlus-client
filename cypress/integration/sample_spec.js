303f23df0a872adbc3946fa4d819621cfdb45315
Hajoo Chung <hajoo.chung@gmail.com>
2020-12-21 10:46:02 +0000

    on testing registration button

--- cypress/integration/sample_spec.js
+++
@@ -1,8 +0,0 @@
-// cypress sample test
-// started writing test to check button on registration page sends to user login page
-
-describe("Registration page button test", () => {
-  it("redirect to user login page", () => {
-    expect(true).to.equal(true);
-  });
-});
