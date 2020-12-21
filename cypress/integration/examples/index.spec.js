76b654e04ac55cfa0fa796980ee9c8864d14ebb3
= <patrickjosephfleming@gmail.com>
2020-12-21 13:48:42 +0000

    Index page set up, coauthored by Ismail

--- cypress/integration/examples/index.spec.js
+++ cypress/integration/examples/index.spec.js
@@ -1,9 +1,11 @@
+//tests that the link to GitHub contains the correct URL
+
 describe('The Index Page GitHub link', () => {
   it('successfully sends user to GitHub', () => {
     //page we're working on
     cy.visit('http://localhost:3000/');
-    cy.get('#gitHubLink') //button/link we're testing
-      .click();
-    cy.location('href').should('eq', 'https://github.com/');
+    cy.get('#gitHubLink')
+      .should('have.attr', 'href')
+      .and('eq', 'https://github.com/');
   });
 });
