c58eca634e159267a5417677e34000514837deb9
= <patrickjosephfleming@gmail.com>
2020-12-22 15:15:56 +0000

    hard coded mastery task fetch req

---
+++ functions/getFeedback.js
@@ -0,0 +1,9 @@
+//function to get the feedback from the backend, may need some refactoring to have consistancy with variable names
+
+async function getFeedback(url = 'http://localhost/5000', uuid, taskType) {
+  const result = await fetch(`${url}/feedback/${uuid}/${taskType}`);
+  const data = await result.json();
+  console.log(data);
+}
+
+export default getFeedback;
--- pages/masteryTasks.js
+++ pages/masteryTasks.js
@@ -2,8 +2,11 @@ import Avatar from '../Components/avatar';
 import UsefulLinks from '../Components/usefulLinks';
 import SignOut from '../Components/signOut';
 import BootcampterListLink from '../Components/bootcamperListLink';
+import getFeedback from '../functions/getFeedback';
 
-export default function Bootcamper() {
+export default function Bootcamper({ data }) {
+  // getFeedback('http://localhost/5000', '1', 'mastery');
+  console.log(data);
   return (
     <div>
       <header className='header'>
@@ -17,3 +20,13 @@ export default function Bootcamper() {
     </div>
   );
 }
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:5000/feedback/1/mastery`);
+  const data = await res.json();
+
+  // Pass data to the page via props
+  return { props: { data } };
+}
+
+//function to get the feedback from the backend, may need some refactoring to have consistancy with variable names
