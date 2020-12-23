fcb29be094b9eb4093b787c3cfdfda55edaff919
Freshta Ebrahim <freshtaebrahim@hotmail.co.uk>
2020-12-23 19:23:32 +0000

    latest edits, context is still not being passed down to bootcampers page

--- Components/authentication/authContext.js
+++ Components/authentication/authContext.js
@@ -1,5 +1,4 @@
-import React, { useContext, useState, createContext, useEffect } from 'react';
-import app from './firebase';
+import React, { useState, createContext, useEffect } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
@@ -9,24 +8,20 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    return firebase.auth().onIdTokenChanged(async (user) => {
+    firebase.auth().onIdTokenChanged(async (user) => {
       if (!user) {
         setUser(null);
+        return;
       }
       const token = await user.getIdToken();
       setUser(user);
       // firebase.auth().onAuthStateChanged(setUser);
-      console.log(user);
     });
   }, []);
-
   console.log(user);
   return (
-    <AuthContext.Provider value={{ user }}>
-      {children}
-      {console.log(user)}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
   );
 };
 
-// export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext);
--- Components/authentication/firebase.js
+++ Components/authentication/firebase.js
@@ -1,5 +1,3 @@
-import firebase from 'firebase/app';
-
 const app = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
@@ -8,15 +6,7 @@ const app = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
 };
-//these are firebase credentials from the firebase account (details are in the .env file)
-
-if (!firebase.apps.length) {
-  try {
-    firebase.initializeApp(app);
-  } catch (err) {
-    console.error('Firebase initialization error raised', err.stack);
-  }
-}
-//this if statement is to prevent firebase constantly rerendering and producing an error
 
 export default app;
+
+//firebase credentials used in app.js
