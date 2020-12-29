3913fb45079e2814146ad48933bff01eca317dcc
Ionut <ipredoi27@gmail.com>
2020-12-29 11:55:46 +0000

    register user function

--- pages/register.js
+++ pages/register.js
@@ -2,6 +2,7 @@
 // register button links to Login page
 import { useAuthContext } from '../firebaseAuthUtils/useAuthContext';
 import '../public/register.css';
+import { useState } from 'react';
 
 import nookies from 'nookies';
 
@@ -9,6 +10,28 @@ import { verifyIdToken } from '../firebaseAuthUtils/firebaseAdmin';
 
 export default function Register({ session }) {
   const { logOut } = useAuthContext();
+  const [role, setRole] = useState('Bootcamper');
+  const [cohort, setCohort] = useState('4');
+  console.log(role);
+  function registerUser(e) {
+    e.preventDefault();
+    fetch('http://localhost:5000/', {
+      method: 'POST',
+      body: JSON.stringify({
+        role: role,
+        uuid: session.uid,
+        cohort: cohort,
+      }),
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      mode: 'cors',
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+    console.log('handlesubmit working');
+  }
 
   if (!session) {
     return (
@@ -38,13 +61,36 @@ export default function Register({ session }) {
           </label>
           <br />
           <label role='role'>SoC Role:</label>
-          <select id='role' name='SocRole'>
+          <select
+            id='role'
+            name='SocRole'
+            onChange={(e) => {
+              setRole(e.target.value);
+            }}>
             <option value='bootcamper'>Bootcamper</option>
             <option value='coach'>Coach</option>
           </select>
           <br />
+          <label cohort='cohort'>SoC Cohort:</label>
+          <select
+            id='cohortNumber'
+            name='socCohort'
+            onChange={(e) => {
+              setCohort(e.target.value);
+            }}>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+            <option value='6'>6</option>
+            <option value='7'>7</option>
+            <option value='8'>8</option>
+            <option value='9'>9</option>
+            <option value='10'>10</option>
+            <option value='11'>11</option>
+            <option value='12'>12</option>
+          </select>
+          <br />
 
-          <button id='form-submit-button' type='submit'>
+          <button id='form-submit-button' type='submit' onClick={registerUser}>
             Submit Form
           </button>
         </form>
