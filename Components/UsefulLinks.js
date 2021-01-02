c42920bec6e7c5866c6a6344082503c3bc02e363
= <patrickjosephfleming@gmail.com>
2021-01-02 10:44:47 +0000

    useful links now rendered from array

--- Components/UsefulLinks.js
+++ Components/UsefulLinks.js
@@ -3,27 +3,17 @@
 
 import { Button } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
+import { linksArr } from '../libs/globalVariables/usefulLinks';
 
 const UsefulLinks = () => (
   <Button.Group className='links' size='large'>
-    <Button href='https://github.com/' target='_blank'>
-      Git Hub
-    </Button>
-    <Button href='http://freecodecamp.org/' target='_blank'>
-      Free Code Camp
-    </Button>
-    <Button href='https://www.codewars.com/' target='_blank'>
-      Code Wars
-    </Button>
-    <Button href='https://stackoverflow.com/' target='_blank'>
-      Stack Overflow
-    </Button>
-    <Button href='https://www.w3schools.com/' target='_blank'>
-      W3 Schools
-    </Button>
-    <Button href='https://developer.mozilla.org/en-US/' target='_blank'>
-      MDN Web Docs
-    </Button>
+    {linksArr.forEach((linksObject) => {
+      return (
+        <Button href={linksObject.link} target='_blank'>
+          {linksObject.title}
+        </Button>
+      );
+    })}
   </Button.Group>
 );
 
