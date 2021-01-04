2557421978878a965e516cfca86188deac7dc80b
= <=>
2021-01-04 11:33:48 +0000

    changed compnent

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,14 +1,14 @@
 //all from semantic ui(avatar)
 //need to work on a drop down onclick to show sign out option
-import styles from '../styles/compnentStyle/avatar.module.css';
+import styles from '../styles/componentStyle/avatar.module.css';
 import React from 'react';
 import { Image } from 'semantic-ui-react';
 
 export default function Avatar({ src, name }) {
   return (
-    <div className='avatar'>
+    <div className="avatar">
       <Image src={src} className={styles.avatar} />
-      <span className='profileName'>{name}</span>
+      <span className="profileName">{name}</span>
     </div>
   );
 }
--- components/LogOutButton.js
+++ components/LogOutButton.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 import { useAuthContext } from '../firebaseAuthUtils/useAuthContext';
-import styles from '../styles/compnentStyle/logOutButton.module.css';
+import styles from '../styles/componentStyle/logOutButton.module.css';
 export default function LogOutButton() {
   const { logOut } = useAuthContext();
   return (
     <div>
-      <Button className={styles.signOutButton} onClick={logOut} color='red'>
+      <Button className={styles.signOutButton} onClick={logOut} color="red">
         Log Out
       </Button>
     </div>
