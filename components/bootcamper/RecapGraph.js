dc7bce610570d129113f06e6482275e8efe86671
Hajoo Chung <hajoo.chung@gmail.com>
2021-01-05 14:44:06 +0000

    mapping data on graph

--- components/bootcamper/RecapGraph.js
+++ components/bootcamper/RecapGraph.js
@@ -4,43 +4,54 @@ import { Bar } from "react-chartjs-2";
 
 export default function RecapGraph({ session }) {
   console.log("data fetch");
-  console.log(session.data.data[0].week); // data from feedback table for uid=d6587569589dk3r437890584gjfni
+  // console.log(session.data.data[0].week);
+  // need to change fetch function, sort merging problem
   // fetch data from backend
+  let feedbackArr = session.data.data;
+  let weekArr = feedbackArr.map((e) => {
+    return [e.week];
+  });
+  let passedTestArr = feedbackArr.map((e) => {
+    return [e.passedtests];
+  });
+  let totalTestArr = feedbackArr.map((e) => {
+    return [e.totaltests];
+  });
+
+  console.log(`feedbackArr: ${feedbackArr}`); // all feedback data from session uid
+  console.log(`weekArr: ${weekArr}`); // week array
+  console.log(`passedTestArr: ${passedTestArr}`); // passed score array
+  console.log(`totalTestArr: ${totalTestArr}`); // total score array
+
   return (
     <div>
       <Bar
         data={{
-          labels: [
-            `week${session.data.data[0].week}`,
-            `week${session.data.data[1].week}`,
-            `week${session.data.data[2].week}`,
-            `week${session.data.data[3].week}`,
-            `week${session.data.data[4].week}`,
-            `week${session.data.data[5].week}`,
-          ],
+          labels: weekArr,
           datasets: [
             {
               label: `session.name's Recap task score [%]`, // name from login session
-              data: [
-                (session.data.data[0].passedtests /
-                  session.data.data[0].totaltests) *
-                  100,
-                (session.data.data[1].passedtests /
-                  session.data.data[1].totaltests) *
-                  100,
-                (session.data.data[2].passedtests /
-                  session.data.data[2].totaltests) *
-                  100,
-                (session.data.data[3].passedtests /
-                  session.data.data[3].totaltests) *
-                  100,
-                (session.data.data[4].passedtests /
-                  session.data.data[4].totaltests) *
-                  100,
-                (session.data.data[5].passedtests /
-                  session.data.data[5].totaltests) *
-                  100,
-              ],
+              data: passedTestArr,
+              // [
+              //   (session.data.data[0].passedtests /
+              //     session.data.data[0].totaltests) *
+              //     100,
+              //   (session.data.data[1].passedtests /
+              //     session.data.data[1].totaltests) *
+              //     100,
+              //   (session.data.data[2].passedtests /
+              //     session.data.data[2].totaltests) *
+              //     100,
+              //   (session.data.data[3].passedtests /
+              //     session.data.data[3].totaltests) *
+              //     100,
+              //   (session.data.data[4].passedtests /
+              //     session.data.data[4].totaltests) *
+              //     100,
+              //   (session.data.data[5].passedtests /
+              //     session.data.data[5].totaltests) *
+              //     100,
+              // ],
               backgroundColor: [
                 "rgba(255, 99, 132, 0.2)",
                 "rgba(54, 162, 235, 0.2)",
