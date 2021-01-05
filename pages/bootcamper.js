5d1dc50b538f741010b72f0d8309726d9a8b4b4c
Freshta Ebrahim <freshtaebrahim@hotmail.co.uk>
2021-01-05 15:48:17 +0000

    student card removed because we don't need it

--- pages/bootcamper.js
+++ pages/bootcamper.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import Avatar from '../components/Avatar';
-
 import UsefulLinks from '../components/usefulLinks';
-import StudentCard from '../components/bootcamper/studentCard';
 import NavBar from '../components/NavBar';
 import QuoteHeader from '../Components/QuoteHeader';
 import { bootcamperNavBarArr } from '../libs/globalVariables/navBarArrays';
@@ -12,20 +10,18 @@ export default function Bootcamper({ session }) {
   if (!session) {
     return (
       <div>
-        <img className="loadingImg" src="/source.gif" alt="loadingImg" />
+        <img className='loadingImg' src='/source.gif' alt='loadingImg' />
       </div>
     );
   } else {
     return (
       <div>
-        <header className="header">
+        <header className='header'>
           <Avatar src={session.picture} name={session.name} />
           <NavBar linksAndTitles={bootcamperNavBarArr} />
         </header>
         <QuoteHeader />
-
-        <StudentCard img={session.picture} />
-        <footer className="footer">
+        <footer className='footer'>
           <UsefulLinks />
         </footer>
       </div>
