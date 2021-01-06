fcf06bd9e9943e811ce43fded13d1d8a066e9f34
= <patrickjosephfleming@gmail.com>
2021-01-06 16:04:35 +0000

    coach page done

---
+++ components/coach/CoachDashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from '../../styles/componentStyle/coachDashboard.module.css';
+
+export default function DashboardDisplay() {
+  return (
+    <div className={styles.container}>
+      <a href='/feedback'>
+        <img className={styles.icons} src='/notes.png' alt='submit feedback' />
+        <p className={styles.link_text}>Submit feedback</p>
+      </a>
+      <a href='/progress'>
+        <img className={styles.icons} src='/diagram.png' alt='check progress' />
+        <p className={styles.link_text}>Check progress</p>
+      </a>
+    </div>
+  );
+}
--- pages/coach.js
+++ pages/coach.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import styles from '../styles/coach.module.css';
-import NavBar from '../components/NavBar';
-import { coachNavBarArr } from '../libs/globalvariables/navBarArrays';
-import Avatar from '../components/avatar';
-import CoachButton from '../components/coach/CoachButton';
-import UsefulLinks from '../components/usefulLinks';
+import styles from '../styles/pagesStyle/coach.module.css';
 import QuoteHeader from '../Components/QuoteHeader';
 import serverSideProps from '../libs/functions/serverSideProps';
 import LoadingImg from '../components/LoadingImg';
 import AppHeader from '../Components/AppHeader';
 import AppFooter from '../Components/AppFooter';
+import { coachNavBarArr } from '../libs/globalVariables/navBarArrays';
+import CoachDashboard from '../Components/coach/CoachDashboard';
 
 export default function Coach({ session }) {
   if (!session) {
     return <LoadingImg />;
   }
   return (
-    <div>
+    <div className={styles.coach}>
       <AppHeader session={session} navBarArr={coachNavBarArr} />
-      <QuoteHeader />
-      <CoachButton />
+      <section className={styles.body}>
+        <div className={styles.quoteHeader}>
+          <QuoteHeader />
+        </div>
+        <h2 className={styles.welcome}>Welcome back, {session.name}</h2>
+        <CoachDashboard />
+      </section>
       <AppFooter />
     </div>
   );
