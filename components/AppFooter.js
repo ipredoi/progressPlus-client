d62d13a6d77846ea7928a3756a0265b2ca7b1fb9
Freshta Ebrahim <freshtaebrahim@hotmail.co.uk>
2021-01-07 15:02:40 +0000

    merge conflict resolved

--- components/AppFooter.js
+++ components/AppFooter.js
@@ -7,44 +7,26 @@ function AppFooter() {
   return (
     <div className={styles.footer}>
       <footer className={styles.container}>
-<<<<<<< HEAD
-        <a href="https://github.com/" target="_blank">
-          <img
-            className={styles.icons}
-            src="/icons/gitHub.png"
-            alt="gitHub image"
-          />
-        </a>
-        <a href="http://freecodecamp.org/" target="_blank">
-          <img
-            className={styles.icons}
-            src="/icons/freecodecamp.png"
-            alt="freecodecamp image"
-          />
-        </a>
-        <a href="https://stackoverflow.com/" target="_blank">
-          <img
-            className={styles.icons}
-            src="/icons/stackoverflow.png"
-            alt="stackoverflow image"
-          />
-=======
         <a href='https://github.com/' target='_blank'>
-          <FontAwesomeIcon icon={fabGithub} />
-          {/* <i
-            aria-hidden='true'
-            className={`fab fa-github fa-3x ${styles.icons}`}></i> */}
+          <img
+            className={styles.icons}
+            src='/icons/gitHub.png'
+            alt='gitHub image'
+          />
         </a>
         <a href='http://freecodecamp.org/' target='_blank'>
-          <i
-            aria-hidden='true'
-            className={`fab fa-free-code-camp fa-3x ${styles.icons}`}></i>
+          <img
+            className={styles.icons}
+            src='/icons/freecodecamp.png'
+            alt='freecodecamp image'
+          />
         </a>
         <a href='https://stackoverflow.com/' target='_blank'>
-          <i
-            aria-hidden='true'
-            className={`fab fa-stack-overflow fa-3x ${styles.icons}`}></i>
->>>>>>> 6aff8c872b082446724c81bbf927adb549efb1a1
+          <img
+            className={styles.icons}
+            src='/icons/stackoverflow.png'
+            alt='stackoverflow image'
+          />
         </a>
       </footer>
     </div>
