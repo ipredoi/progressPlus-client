3b8da5b8fa96f1076e483e92da4c5645ca37e337
Freshta Ebrahim <freshtaebrahim@hotmail.co.uk>
2021-01-07 15:04:48 +0000

    merge conflicts resolved pt 2

--- components/AppFooter.js
+++ components/AppFooter.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styles from '../styles/componentStyle/appFooter.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { fabGithub } from '@fortawesome/free-solid-svg-icons';
 
 function AppFooter() {
   return (
