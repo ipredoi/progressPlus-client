e6d7d5f69dd01e152e44a2de763a2a3a282c0947
unknown <freshtaebrahim@hotmail.co.uk>
2021-01-12 10:40:41 +0000

    new footer and nav bar

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -8,30 +8,65 @@ export default function NavBar({ linksAndTitles }) {
   const { open, setOpen } = useAuthContext();
   return (
     <div>
-      <Sidebar as={Segment} animation='overlay' direction='top' visible={open}>
+      <Sidebar
+        as={Segment}
+        animation='scale down'
+        direction='left'
+        visible={open}>
+        {/* <Menu.item>
+          {linksAndTitles.map((object) => (
+            <Menu.item key={object.title} href={object.link}>
+              <Header>{object.title}</Header>
+            </Menu.item>
+          ))}
+        </Menu.item> */}
+
         <Grid textAlign='center'>
-          <Grid.Row columns={1}>
-            <Grid.Column></Grid.Column>
-          </Grid.Row>
-          <Grid.Row columns={linksAndTitles.length + 1}>
+          {/* <Grid.Column Columns={1}>
+            <Grid.Row></Grid.Row>
+          </Grid.Column> */}
+          <Grid.Column Rows={linksAndTitles.length + 1}>
             {linksAndTitles.map((object) => (
-              <Grid.Column key={object.title} href={object.link}>
+              <Grid.Row key={object.title} href={object.link}>
                 <Header>{object.title}</Header>
-              </Grid.Column>
+                <Grid.Column Columns={1}>
+                  <Grid.Row></Grid.Row>
+                </Grid.Column>
+              </Grid.Row>
             ))}
-            <Grid.Column>
-              {/* put in edit profile component or redirect to edit profile page */}
+            <Grid.Row>
               <LogOutButton />
-            </Grid.Column>
-          </Grid.Row>
-          <Button
-            onClick={() => {
-              setOpen(!open);
-            }}>
-            ^
-          </Button>
+            </Grid.Row>
+          </Grid.Column>
         </Grid>
       </Sidebar>
     </div>
   );
+}
+
+{
+  /* <Button
+            onClick={() => {
+              setOpen(!open);
+            }}></Button> */
+}
+// {
+//   <Grid textAlign='center'>
+//           <Grid.Column Columns={1}>
+//             <Grid.Row></Grid.Row>
+//           </Grid.Column>
+//           <Grid.Column Rows={linksAndTitles.length + 1}>
+//             {linksAndTitles.map((object) => (
+//               <Grid.Row key={object.title} href={object.link}>
+//                 <Header>{object.title}</Header>
+//               </Grid.Row>
+//             ))}
+//             <Grid.Row>
+//               put in edit profile component or redirect to edit profile page
+// }
+{
+  /* <LogOutButton />
+            </Grid.Row>
+          </Grid.Column>
+        </Grid>  */
 }
