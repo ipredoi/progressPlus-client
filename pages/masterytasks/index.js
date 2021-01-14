2f2ca5969364d834ea55aa1a65bfe3ae500fb251
unknown <freshtaebrahim@hotmail.co.uk>
2021-01-14 22:28:43 +0000

    added white background/cards to bootcamper graphs but struggling to center

--- pages/masterytasks/index.js
+++ pages/masterytasks/index.js
@@ -8,21 +8,22 @@ import styles from './masterytasks.module.css';
 export default function MasteryTasks({ session }) {
   const [selectedData, setSelectedData] = useState(1);
   return (
-    <div>
+    <>
       <AppHeader session={session} />
-
-      <div className={styles.graph}>
-        <ScoreGraph
-          feedbackData={session.data}
-          setSelectedData={setSelectedData}
-          taskType='Mastery'
-          myName={session.name}
-        />
+      <div className={styles.masteryTasks}>
+        <div className={styles.graph}>
+          <ScoreGraph
+            feedbackData={session.data}
+            setSelectedData={setSelectedData}
+            taskType='Mastery'
+            myName={session.name}
+          />
+        </div>
+        <div className={styles.table}>
+          <FeedbackTable selectedData={selectedData} />
+        </div>
       </div>
-      <div className={styles.table}>
-        <FeedbackTable selectedData={selectedData} />
-      </div>
-    </div>
+    </>
   );
 }
 
