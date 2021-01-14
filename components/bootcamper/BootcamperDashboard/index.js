cdf532870218255e4258702e3398345e57a1bdc3
Ionut <ipredoi27@gmail.com>
2021-01-14 23:53:57 +0000

    Links added

--- components/bootcamper/BootcamperDashboard/index.js
+++ components/bootcamper/BootcamperDashboard/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import styles from './bootcamperDashboard.module.css';
-
+import Link from 'next/link';
 export default function BootcamperDashboard() {
   return (
     <div className={styles.container}>
-      <a href="/masterytasks" className={styles.imageLink}>
-        <img
-          className={styles.icons}
-          src="/diagram.png"
-          alt="mastery graph image"
-        />
-        <p className={styles.link_text}>Mastery tasks</p>
-      </a>
-      <a href="/recaptasks" className={styles.imageLink}>
-        <img
-          className={styles.icons}
-          src="/bar-chart.png"
-          alt="recap graph image"
-        />
-        <p className={styles.link_text}>Recap tasks</p>
-      </a>
+      <Link href='/masterytasks' className={styles.imageLink}>
+        <a>
+          <img
+            className={styles.icons}
+            src='/diagram.png'
+            alt='mastery graph image'
+          />
+          <p className={styles.link_text}>Mastery tasks</p>
+        </a>
+      </Link>
+      <Link href='/recaptasks' className={styles.imageLink}>
+        <a>
+          <img
+            className={styles.icons}
+            src='/bar-chart.png'
+            alt='recap graph image'
+          />
+          <p className={styles.link_text}>Recap tasks</p>
+        </a>
+      </Link>
     </div>
   );
 }
--- components/coach/CoachDashboard/index.js
+++ components/coach/CoachDashboard/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import styles from './coachDashboard.module.css';
-
+import Link from 'next/link';
 export default function CoachDashboard() {
   return (
     <div className={styles.container}>
-      <a className={styles.imageLink} href="/feedback">
-        <img className={styles.icons} src="/notes.png" alt="submit feedback" />
-        <p className={styles.link_text}>Submit feedback</p>
-      </a>
-      <a href="/progress" className={styles.imageLink}>
-        <img className={styles.icons} src="/diagram.png" alt="check progress" />
-        <p className={styles.link_text}>Check progress</p>
-      </a>
+      <Link className={styles.imageLink} href='/feedback'>
+        <a>
+          <img
+            className={styles.icons}
+            src='/notes.png'
+            alt='submit feedback'
+          />
+          <p className={styles.link_text}>Submit feedback</p>
+        </a>
+      </Link>
+      <Link href='/progress' className={styles.imageLink}>
+        <a>
+          <img
+            className={styles.icons}
+            src='/diagram.png'
+            alt='check progress'
+          />
+          <p className={styles.link_text}>Check progress</p>
+        </a>
+      </Link>
     </div>
   );
 }
