934d4e332ed532d356b5d811e2cd19ee298c9d11
= <=>
2021-01-14 17:25:38 +0000

    css

--- components/loginBackgroundImg/LoginBackgroundImg.js
+++ components/loginBackgroundImg/LoginBackgroundImg.js
@@ -7,19 +7,19 @@ const LoginBackgroundImg = () => {
     <div>
       <div className={styles.planetSocImg}>
         <Image
-          src='/planet_soc.png'
-          alt='School of Code Logo'
-          width={450}
+          src="/planet_soc.png"
+          alt="School of Code Logo"
+          width={400}
           height={450}
         />
       </div>
       <div className={styles.moonSocImg}>
-        <Image src='/moon.png' alt='Moon Image' layout='fill' />
+        <Image src="/moon.png" alt="Moon Image" layout="fill" />
       </div>
       <div className={styles.astronautImg}>
         <Image
-          src='/astronaut.png'
-          alt='School of Code Logo'
+          src="/astronaut.png"
+          alt="School of Code Logo"
           width={450}
           height={450}
         />
