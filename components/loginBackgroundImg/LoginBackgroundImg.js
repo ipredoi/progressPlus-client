1da3f71c0be4cfa8c99db8df5d84816fa99606eb
Ionut <ipredoi27@gmail.com>
2021-01-14 16:19:32 +0000

    register & l git commit -m register

---
+++ components/loginBackgroundImg/LoginBackgroundImg.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './loginBackgroundImg.module.css';
+import Image from 'next/image';
+
+const LoginBackgroundImg = () => {
+  return (
+    <div>
+      <div className={styles.planetSocImg}>
+        <Image
+          src='/planet_soc.png'
+          alt='School of Code Logo'
+          width={450}
+          height={450}
+        />
+      </div>
+      <div className={styles.moonSocImg}>
+        <Image src='/moon.png' alt='Moon Image' layout='fill' />
+      </div>
+      <div className={styles.astronautImg}>
+        <Image
+          src='/astronaut.png'
+          alt='School of Code Logo'
+          width={450}
+          height={450}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginBackgroundImg;
--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,11 @@ import FirebaseAuth from '../components/authentication/Firebase';
 import styles from './index.module.css';
 import Image from 'next/image';
 import { appName } from '../libs/globalVariables/appName';
-
+import LoginBackgroundImg from '../components/loginBackgroundImg/LoginBackgroundImg';
 export default function Login() {
   return (
     <div className={styles.auth}>
-      <div className={styles.planetSocImg}>
-        <Image
-          src='/planet_soc.png'
-          alt='School of Code Logo'
-          width={500}
-          height={500}
-        />
-      </div>
-      <div className={styles.moonSocImg}>
-        <Image src='/moon.png' alt='School of Code Logo' layout='fill' />
-      </div>
-
+      <LoginBackgroundImg />
       <div className={styles.authMain}>
         <div className={styles.socImage}>
           <Image
