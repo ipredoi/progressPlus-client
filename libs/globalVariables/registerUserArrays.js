d9d8be233fdb652062530dd029a0983b158cc77c
Ismail <ismailyma01@gmail.com>
2021-01-14 17:36:26 +0000

    hamburger&;cohort

--- libs/globalVariables/registerUserArrays.js
+++ libs/globalVariables/registerUserArrays.js
@@ -16,7 +16,7 @@ const rolesArr = [
 // generatin an array with numbers from cohort 4-99
 
 let cohortArr = [];
-for (let i = 4; i < 100; i++) {
+for (let i = 4; i < 6; i++) {
   cohortArr.push({
     key: i,
     text: `${i}`,
