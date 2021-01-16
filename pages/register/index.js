31c967ba21926ab0b4986a0759cec5a56553ef9b
unknown <freshtaebrahim@hotmail.co.uk>
2021-01-16 15:44:31 +0000

    redeploy

--- pages/register/index.js
+++ pages/register/index.js
@@ -6,11 +6,7 @@ import nookies from 'nookies';
 import useFormSubmit from '../../libs/customHooks/useFormSubmit';
 import validateRegisterForm from '../../libs/functions/Register/validateRegisterForm';
 import { verifyIdToken } from '../../firebaseUtils/firebaseAdmin';
-<<<<<<< HEAD
-import LoginBackgroundImg from '../../components/loginBackgroundImg/LoginBackgroundImg';
-=======
 import LoginBackgroundImg from '../../components/LoginBackgroundImg';
->>>>>>> dev
 import DropdownMenu from '../../components/authentication/DropdownMenu';
 import InputField from '../../components/authentication/InputField';
 import RegisterButton from '../../components/authentication/RegisterButton';
