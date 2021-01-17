a4ea7921e6d8285138ef7ade50e419d47b702053
= <=>
2021-01-17 03:07:19 +0000

    folder name

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { Grid, Header, Segment, Sidebar, Button } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
+import '../coach/FeedbackForm/node_modules/semantic-ui-css/semantic.min.css';
 import LogOutButton from '../LogOutButton';
 import { useAuthContext } from '../../firebaseUtils/useAuthContext';
 import styles from './navBar.module.css';
@@ -32,15 +32,15 @@ export default function NavBar({ linksAndTitles }) {
     <div className={styles.container} ref={navBar}>
       <Sidebar
         as={Segment}
-        animation='scale down'
-        direction='left'
+        animation="scale down"
+        direction="left"
         visible={open}
         className={styles.nav}>
-        <Grid textAlign='center'>
+        <Grid textAlign="center">
           <Grid.Column rows={linksAndTitles.length}>
             {linksAndTitles.map((object) => (
               <Grid.Row key={object.title} href={object.link}>
-                <Header className='nav-link'>{object.title}</Header>
+                <Header className="nav-link">{object.title}</Header>
                 <Grid.Column>
                   <Grid.Row></Grid.Row>
                 </Grid.Column>
